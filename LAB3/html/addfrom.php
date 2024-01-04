11c9b2f86dfd31b4e905f44476cb1c8f2cd6bd72
ThanandonSodnangrong <650112418007@bru.ac.th>
2024-01-04 15:01:27 +0700

    LAB7

--- LAB3/html/addfrom.php
+++ LAB3/html/addfrom.php
@@ -1,34 +1,31 @@
 <form>
-    <label for="input_id">ID :</label>
+<label for="input_id">ID </label><br>
     <input type="text" name="id" id="input_id"><br>
-
-    <label for="input_name">NAME :</label>
+    <label for="input_name">NAME</label><br>
     <input type="text" name="name" id="input_name"><br>
-
-    <label for="input_prov">PROVINCH :</label>
+    <label for="input_pro">PROVINCE</label><br>
     <input type="text" name="prov" id="input_prov"><br>
-
-    <button type="submit">SAVE</button>
-    <button type="reset">CANCLE</button>
+    <hr>
+    <button type="submit" class="btn btn-primary" >=> SAVE <=</button>
+    <button type="reset" class="btn btn-danger" >=> CANCEL <=</button>
 </form>
 
 
 <script>
-    $("form").submit(function(e) {
+    $("form").submit(function(e){
         e.preventDefault();
-
-        let fm = $(this);
+        let form = $(this);
         $.ajax({
             url: "/additem.php",
             method: "POST",
-            data: fm.serialize(),
+            data: form.serialize(),
             success: function(res) {
-                console.log(res);
-
-                if (res == "error")
-                    alert("Don't insert data into DB.");
-                else
+                if(res == "error"){
+                    alert("Don't insert data to DB");
+                }else{
                     $("#div_item").load("/listitem.php");
+                    $('#staticBackdropLabel').modal('hide');
+                }
             }
         });
     });
--- LAB3/html/listitem.php
+++ LAB3/html/listitem.php
@@ -1,65 +1,80 @@
 <?php
-    include "condb.php" ; 
+include "condb.php";
 
-    $sql = "SELECT * FROM tb_member";
+$sql = "SELECT * FROM tb_member";
+$result = mysqli_query($conn, $sql);
 
-    $result = mysqli_query($conn,$sql); 
-
-   // var_dump($result);
-
+// var_dump($result);
 ?>
+<!-- Button trigger modal -->
+<button id="btn_add" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+    + ADD
+</button>
 
-<button id="btn_add">Add Data</button>
-
-<table>
+<table class="table table-striped table-hover">
     <thead>
         <tr>
-            <th>ID MEMBER</th>
-            <th>NAME</th>
+            <th>ID</th>
+            <th>Name</th>
             <th>PROVINCE</th>
-            <th>Action</th>
+            <th>ACTION</th>
+            <th></th>
+            <th></th>
         </tr>
     </thead>
     <tbody>
         <?php
-        while($row = mysqli_fetch_assoc($result)){
+        while ($row = mysqli_fetch_assoc($result)) {
         ?>
-        <tr>
-            <td><?= $row["id_member"] ?></td>
-            <td><?= $row["member_name"] ?></td>
-            <td><?= $row["id_province"] ?></td>
-            <td><button class="btn_del" data-id="<?= $row["id_member"] ?>">Delete</button></td>
-        </tr>
+            <tr>
+                <td><?= $row['id_member'] ?></td>
+                <td><?= $row['name'] ?></td>
+                <td><?= $row['id_province'] ?></td>
+                <td><button class="btn_del btn btn-danger" data-id="<?= $row['id_member'] ?>">del</button></td>
+                <td>
+                    <button class="btn_edt btn btn-info" data-id="<?= $row['id_member'] ?>" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Edit</button>
+                </td>
+            </tr>
         <?php
         }
         ?>
     </tbody>
 </table>
 
+<script>
+    $(function() {
+        $(".btn_edt").click(function() {
+            let id = $(this).data('id');
+            console.log(id);
 
-<script> //JS
-    $(".btn_del").click(function(){
-        let id = $(this).data("id"); //id(js) -> data-id(php)
-        console.log(id);
-
-        $.ajax({
-            url:"/dellitem.php",
-            method:"GET",
-            data:{
-                id_mem: id 
-            },
-            success:function(res){
-                console.log(res);
-                if(res == "error")
-                alert("Can't delete item.");
-                else 
-                $("#div_item").load("/listitem.php");
-            }
+            $("#staticBackdropLabel").text("Edit member");
+            $(".modal-body").load(`/editform.php?id=${id}`);
+            $(".modal-footer").hide();
         });
-    });
 
-    $("#btn_add").click(function(){
-        $("#div_item").load("/addfrom.php");
-    });
+        $(".btn_del").click(function() {
+            let id = $(this).data('id');
+            console.log(id);
 
+            $.ajax({
+                url: "/dellitem.php",
+                method: "GET",
+                data: {
+                    mem_id: id
+                },
+                success: function(res) {
+                    if (res == 'error')
+                        alert("Can't delete item.");
+                    else
+                        $("#div_item").load("/listitem.php");
+                }
+            });
+        });
+
+        $("#btn_add").click(function() {
+            $("#staticBackdropLabel").text("Add member");
+            $(".modal-body").load("/addfrom.php");
+            $(".modal-footer").hide();
+        });
+    }); //jQuery Ready
 </script>
